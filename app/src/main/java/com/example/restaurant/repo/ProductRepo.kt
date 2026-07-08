1343c8c777d10a22258948172e2d02af5bfe8d71
Nitish <nitishstha15@gmail.com>
2026-07-08 09:57:44 +0545

    Update ProductRepo

--- app/src/main/java/com/example/restaurant/repo/ProductRepo.kt
+++ app/src/main/java/com/example/restaurant/repo/ProductRepo.kt
@@ -13,23 +13,17 @@ interface ProductRepo {
 //    "message": "Product fetched succesfully"
 //}
 //
-    fun addProduct
-                (model: ProductModel,callback:(Boolean,String) -> Unit)
+    fun addProduct(model: ProductModel,callback:(Boolean,String) -> Unit)
 
-    fun updateProduct
-                (model: ProductModel,callback: (Boolean, String) -> Unit)
+    fun updateProduct(model: ProductModel,callback: (Boolean, String) -> Unit)
 
-    fun deleteProduct
-                (productId:String,callback: (Boolean, String) -> Unit)
+    fun deleteProduct(productId:String,callback: (Boolean, String) -> Unit)
 
-    fun getAllProduct
-                (callback: (Boolean, String, List<ProductModel>?) -> Unit)
+    fun getAllProduct(callback: (Boolean, String, List<ProductModel>?) -> Unit)
 
-    fun getProductById
-                (productId: String,callback: (Boolean, String, ProductModel?) -> Unit)
+    fun getProductById(productId: String,callback: (Boolean, String, ProductModel?) -> Unit)
 
-    fun getProductByCategory
-                (categoryId:String, callback: (Boolean, String, List<ProductModel>?) -> Unit)
+    fun getProductByCategory(categoryId:String, callback: (Boolean, String, List<ProductModel>?) -> Unit)
 
     fun uploadImage(context: Context, imageUri: Uri, callback: (String?) -> Unit)
 
