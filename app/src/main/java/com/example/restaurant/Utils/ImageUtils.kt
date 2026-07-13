36bd1386ab13de415c162f5124e98c5913535c8b
Nitish <nitishstha15@gmail.com>
2026-07-13 20:53:05 +0545

    Final updates before push

--- app/src/main/java/com/example/restaurant/Utils/ImageUtils.kt
+++ app/src/main/java/com/example/restaurant/Utils/ImageUtils.kt
@@ -13,6 +13,7 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
 import android.content.pm.PackageManager
 
+
 class ImageUtils(private val activity: Activity, private val registryOwner: ActivityResultRegistryOwner) {
     private lateinit var galleryLauncher: ActivityResultLauncher<Intent>
     private lateinit var permissionLauncher: ActivityResultLauncher<String>
